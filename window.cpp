fc5fd44b98efa258204841755ca35be1f7ddf62d
Robin Hill <rehill012@gmail.com>
2013-03-09 00:36:05 -0500

    - working version of print, with no color - added a few comments

--- window.cpp
+++ window.cpp
@@ -1,4 +1,5 @@
 #include "globals.h"
+#include "stringutils.h"
 #include "window.h"
 
 using namespace std;
@@ -7,6 +8,7 @@ window::window(int n_x, int n_y, int n_w, int n_h) : x(n_x), y(n_y), width(n_w),
 
 }
 
+// Clear the window
 void window::clear(){
 
 	for(int j = y; j < height + y; ++j){
@@ -17,6 +19,7 @@ void window::clear(){
 	}
 }
 
+// Display the player's map
 void window::display_map(map * m){
 
 	for(int i = 0; i < height; ++i){
@@ -30,6 +33,7 @@ void window::display_map(map * m){
 	}
 }
 
+// Display player's status
 void window::display_status(){
 
 	clear();
@@ -43,14 +47,42 @@ void window::display_status(){
 	printw("$%d", act_player->gold);
 }
 
+// Print a string
 void window::print(string text){
-	print(text, buf_main);
+	print(text, *buf_main);
 }
 
-void window::print(string text, buffer * buf){
+// Add a string to the buffer and print it
+void window::print(string text, buffer & buf){
 
 	clear();
 	move(0, 0);
+	buf.push_back(text);
+	print_buf(buf);
+}
+
+// Print as much the buffer as will fit in this window
+void window::print_buf(buffer & buf){
+	int screen_pointer = 0, buffer_pointer = 0;
 	
+	while(buffer_pointer < buf.size()){
+		
+		string next = buf[buf.size() - buffer_pointer++ - 1];
+		vector<string> cut = string_slice(next, width);
+		
+		for(int i = cut.size() - 1; i >= 0 && screen_pointer < height; --i){
+			print_line(cut[i], height - screen_pointer++ - 1);
+		}
+	}
+	
+}
+
+void window::print_line(string in, int pos){
+	move(y + pos, x);
+	
+	int len = in.size();
+	for(int i = 0; i < len; ++i){
+		addch(in.at(i));
+	}
 
 }
