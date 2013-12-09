7eea4403ce20270b31b0091b403bb687a7b8c1c7
Christopher Hill <sttattic@gmail.com>
2013-12-08 23:42:21 -0500

    Added argmap and conditions

---
+++ condition.h
@@ -0,0 +1,20 @@
+#ifndef CONDITION_H
+
+#define CONDITION_H
+
+#include "effect.h"
+
+class condition {
+
+	public:
+	
+	int type;
+	int stack;
+	int time;
+
+	
+	//Functions:
+	condition(int, int, int);
+};
+
+#endif
